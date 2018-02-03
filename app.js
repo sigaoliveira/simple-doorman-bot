84a9016a3b46eb1ae743ea24869d52011ff68843
sigaoliveira <rodrigoco@gmail.com>
2018-02-03 00:37:07 -0200

    whois new bug

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`);
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`);
 });
 
 // Create an event listener for new guild members
@@ -167,7 +167,7 @@ client.on("message", async message => {
 	message.reply("Scanning....");
     message.channel.send(`User ID#: ${member.id}`)
     message.channel.send(`Nickname: ${member.nickname}`)
-	message.channel.send(`Username: ${user.username}`)
+	message.channel.send(`Username: ${member.user.username}`)
   }
   
 	if(command === "nick") {
