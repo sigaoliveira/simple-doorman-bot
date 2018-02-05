475a32e586e0c363456b6d14c3b80102582bfaec
sigaoliveira <rodrigoco@gmail.com>
2018-02-05 01:02:08 -0200

    lets try again

--- app.js
+++ app.js
@@ -207,7 +207,8 @@ client.on("message", async message => {
     // slice(1) removes the first part, which here should be the user mention!
     let alliance = args[0];
 	console.log(`This is the alliance: ${alliance} and ${args}`) 
-    let castle = args.slice(1).join(' ').shift().toLowerCase();
+    let castles = args.slice(1).join(' ');
+	const caste = castles.shift().toLowerCase()
 	console.log(`This is the castle: ${castle}`)
 	
     if(!alliance || alliance !== "s" || alliance !== "z")
